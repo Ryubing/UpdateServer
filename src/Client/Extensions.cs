e75044025c8db600dcb0975d34c26603198da051
GreemDev <greemdev@ryujinx.app>
2025-06-15 04:41:30 -0500

    Simple write to file extension for Streams to complement DownloadAsync

---
+++ src/Client/Extensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Ryujinx.Systems.Update.Client;
+
+public static class Extensions
+{
+    /// <summary>
+    ///     Sets the position in the current <see cref="Stream"/> to 0 and write the entire contents of it to the file specified at the path.
+    /// </summary>
+    /// <param name="stream">The stream to copy data from.</param>
+    /// <param name="filePath">The target file path to copy the stream's contents. Will be created if it doesn't exist.</param>
+    public static async Task WriteToFileAsync(this Stream stream, string filePath)
+    {
+        stream.Seek(0, SeekOrigin.Begin);
+        await using var f = File.OpenWrite(filePath);
+        await stream.CopyToAsync(f);
+    }
+}
