085fce0ab26b82287e5db76bb581c589ee5c6186
GreemDev <greemdev@ryujinx.app>
2025-12-23 23:32:57 -0600

    add base of URI redirects for helpful things

---
+++ src/Server/Controllers/IndexController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ryujinx.Systems.Update.Server.Helpers;
+
+namespace Ryujinx.Systems.Update.Server.Controllers;
+
+[Route("/")]
+[ApiController]
+public class IndexController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult Index() 
+        => Redirect("https://github.com/Ryubing/UpdateServer/");
+
+    [HttpGet("docs"), HttpGet("info"), HttpGet("help")]
+    public ActionResult Help()
+    {
+        return CommandLineState.UseSwagger 
+            ? Redirect("/swagger") 
+            : Index();
+    }
+}
