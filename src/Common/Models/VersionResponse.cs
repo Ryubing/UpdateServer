5b15456a749a479e851b55c8d6d13a46d277b606
GreemDev <greemdev@ryujinx.app>
2026-03-31 03:29:06 -0500

    remove deprecated web_url_format version response property

--- src/Common/Models/VersionResponse.cs
+++ src/Common/Models/VersionResponse.cs
@@ -7,16 +7,13 @@ namespace Ryujinx.Systems.Update.Common;
 
 public class VersionResponse
 {
-    [JsonPropertyName("tag")]
-    public required string Version { get; set; }
-    
-    [JsonPropertyName("download_url")]
-    public required string ArtifactUrl { get; set; }
-    
-    [JsonPropertyName("web_url")] public string ReleaseUrl  => string.Format(ReleaseUrlFormat, Version);
-    
-    [JsonPropertyName("web_url_format")]
-    public required string ReleaseUrlFormat { get; set; }
+    [JsonPropertyName("tag")] public required string Version { get; set; }
+
+    [JsonPropertyName("download_url")] public required string ArtifactUrl { get; set; }
+
+    [JsonPropertyName("web_url")] public string ReleaseUrl => string.Format(ReleaseUrlFormat, Version);
+
+    [JsonIgnore] public string ReleaseUrlFormat { get; set; }
 }
 
 public class DownloadLinks
@@ -24,6 +21,7 @@ public class DownloadLinks
     [JsonPropertyName("windows")] public required SupportedPlatform Windows { get; init; }
     [JsonPropertyName("linux")] public required SupportedPlatform Linux { get; init; }
     [JsonPropertyName("linux_appimage")] public required SupportedPlatform LinuxAppImage { get; init; }
+
     // ReSharper disable once InconsistentNaming
     [JsonPropertyName("macOS")] public required string MacOS { get; init; }
 
